81e3a6bdf0d6250eb3c303563bdb6ad5f6b3d144
Ash Shah <142115500+Aasna-S@users.noreply.github.com>
2026-09-08 12:30:57 -0400

    Update 01_setup.sql

--- sql/01_setup.sql
+++ sql/01_setup.sql
@@ -3,10 +3,9 @@ Research Administration Reporting Modernization
 Snowflake SQL portfolio project
 
 Execution order is reflected in the filenames.
-- Source/reference CSVs are assumed to have already been loaded into
+- Source/reference CSVs are loaded into
   RESEARCH_ADMIN_ANALYTICS.STAGING as source tables.
-- The SQL does not pretend to implement production ingestion/orchestration.
-- In the portfolio, final reporting outputs were exported to CSV for Tableau.
+- final reporting outputs were exported to CSV for Tableau.
 */
 
 -- ============================================================
