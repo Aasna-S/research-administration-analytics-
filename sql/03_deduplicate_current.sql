29f5e144b04eadd1b8797745e47d46697aa23128
Ash Shah <142115500+Aasna-S@users.noreply.github.com>
2026-09-08 12:31:43 -0400

    Update 03_deduplicate_current.sql

--- sql/03_deduplicate_current.sql
+++ sql/03_deduplicate_current.sql
@@ -1,12 +1,6 @@
 /*
 Research Administration Reporting Modernization
 Snowflake SQL portfolio project
-
-Execution order is reflected in the filenames.
-- Source/reference CSVs are assumed to have already been loaded into
-  RESEARCH_ADMIN_ANALYTICS.STAGING as source tables.
-- The SQL does not pretend to implement production ingestion/orchestration.
-- In the portfolio, final reporting outputs were exported to CSV for Tableau.
 */
 
 -- Purpose:
